65b316397d9340106b6bb2fafbc317964693bd97
Brian Egan <brian@brianegan.com>
2018-05-31 04:16:19 +0200

    Convert the Counter app to a sample that can be used to test bugs with dismissable and dispatching too many actions

--- example/counter/lib/main.dart
+++ example/counter/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
 
@@ -20,87 +22,112 @@ void main() {
   // with Hot Reload than creating it directly in the `build` function.
   final store = new Store<int>(counterReducer, initialState: 0);
 
-  runApp(new FlutterReduxApp(
-    title: 'Flutter Redux Demo',
+  runApp(new MyApp(
+    items: new List<String>.generate(100, (i) => "Item ${i + 1}"),
     store: store,
   ));
 }
 
-class FlutterReduxApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   final Store<int> store;
-  final String title;
+  final List<String> items;
 
-  FlutterReduxApp({Key key, this.store, this.title}) : super(key: key);
+  MyApp({
+    Key key,
+    @required this.items,
+    @required this.store,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // The StoreProvider should wrap your MaterialApp or WidgetsApp. This will
-    // ensure all routes have access to the store.
+    final title = 'Dismissing Items';
+
     return new StoreProvider<int>(
-      // Pass the store to the StoreProvider. Any ancestor `StoreConnector`
-      // Widgets will find and use this value as the `Store`.
       store: store,
-      child: new MaterialApp(
-        theme: new ThemeData.dark(),
-        title: title,
-        home: new Scaffold(
-          appBar: new AppBar(
-            title: new Text(title),
-          ),
-          body: new Center(
-            child: new Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                new Text(
-                  'You have pushed the button this many times:',
-                ),
-                // Connect the Store to a Text Widget that renders the current
-                // count.
-                //
-                // We'll wrap the Text Widget in a `StoreConnector` Widget. The
-                // `StoreConnector` will find the `Store` from the nearest
-                // `StoreProvider` ancestor, convert it into a String of the
-                // latest count, and pass that String  to the `builder` function
-                // as the `count`.
-                //
-                // Every time the button is tapped, an action is dispatched and
-                // run through the reducer. After the reducer updates the state,
-                // the Widget will be automatically rebuilt with the latest
-                // count. No need to manually manage subscriptions or Streams!
-                new StoreConnector<int, String>(
-                  converter: (store) => store.state.toString(),
-                  builder: (context, count) {
-                    return new Text(
-                      count,
-                      style: Theme.of(context).textTheme.display1,
+      child: new InputDetector(
+        onInput: () => store.dispatch('Input Detected'),
+        child: new MaterialApp(
+          title: title,
+          home: new Scaffold(
+            appBar: new AppBar(
+              title: new Text(title),
+            ),
+            body: new StoreConnector<int, int>(
+              converter: (store) => store.state,
+              builder: (store, vm) {
+                return new ListView.builder(
+                  itemCount: items.length,
+                  itemBuilder: (context, index) {
+                    final item = items[index];
+
+                    return new Dismissible(
+                      // Each Dismissible must contain a Key. Keys allow Flutter to
+                      // uniquely identify Widgets.
+                      key: new Key(item),
+                      // We also need to provide a function that will tell our app
+                      // what to do after an item has been swiped away.
+                      onDismissed: (direction) {
+                        items.removeAt(index);
+
+                        Scaffold.of(context).showSnackBar(
+                            new SnackBar(content: new Text("$item dismissed")));
+                      },
+                      // Show a red background as the item is swiped away
+                      background: new Container(color: Colors.red),
+                      child: new ListTile(title: new Text('$item')),
                     );
                   },
-                )
-              ],
+                );
+              },
             ),
-          ),
-          // Connect the Store to a FloatingActionButton. In this case, we'll
-          // use the Store to build a callback that with dispatch an Increment
-          // Action.
-          //
-          // Then, we'll pass this callback to the button's `onPressed` handler.
-          floatingActionButton: new StoreConnector<int, VoidCallback>(
-            converter: (store) {
-              // Return a `VoidCallback`, which is a fancy name for a function
-              // with no parameters. It only dispatches an Increment action.
-              return () => store.dispatch(Actions.Increment);
-            },
-            builder: (context, callback) {
-              return new FloatingActionButton(
-                // Attach the `callback` to the `onPressed` attribute
-                onPressed: callback,
-                tooltip: 'asdasdasd',
-                child: new Icon(Icons.add),
-              );
-            },
           ),
         ),
       ),
     );
   }
+}
+
+class InputDetector extends StatefulWidget {
+  final VoidCallback onInput;
+  final Widget child;
+
+  InputDetector({@required this.child, @required this.onInput})
+      : assert(child != null),
+        assert(onInput != null);
+
+  @override
+  State<StatefulWidget> createState() => new _InputDetectorState();
+}
+
+class _InputDetectorState extends State<InputDetector> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Detect raw input from the keyboard (alas, only the hardware keyboard).
+    RawKeyboard.instance.addListener(_onRawKeyEvent);
+  }
+
+  @override
+  void dispose() {
+    RawKeyboard.instance.removeListener(_onRawKeyEvent);
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var touchDetector = new Listener(
+      behavior: HitTestBehavior.translucent,
+      child: widget.child,
+      onPointerDown: (_) => widget.onInput(),
+      onPointerMove: (_) => widget.onInput(),
+      onPointerCancel: (_) => widget.onInput(),
+      onPointerUp: (_) => widget.onInput(),
+    );
+
+    return touchDetector;
+  }
+
+  void _onRawKeyEvent(RawKeyEvent event) => widget.onInput();
 }
